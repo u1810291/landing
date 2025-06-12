82616fde08e99d10f94b818d9d06d52cb113502e
u1810291 <otabekbutcher@gmail.com>
2025-06-13 03:46:19 +0400

    feat: added yandex metric for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
 import Script from 'next/script';
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID
+const YANDEX_METRICA_ID = process.env.NEXT_PUBLIC_YANDEX_METRICA_ID
 
 export default function RootLayout({
   children,
@@ -29,6 +30,39 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
       <head>
+        {/* Yandex analytics */}
+        <Script
+          id="yandex-metrica"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){
+                      (m[i].a=m[i].a||[]).push(arguments)};
+                      m[i].l=1*new Date();
+                      k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                      k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(${YANDEX_METRICA_ID}, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${YANDEX_METRICA_ID}`}
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* Google analytics */}
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           strategy="afterInteractive"
